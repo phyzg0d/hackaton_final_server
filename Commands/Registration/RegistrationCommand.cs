4e223e9d9cdf0d6ee6604fa13e0c2a94b130cf77
phyzg0d <mephyz@mail.ru>
2020-11-28 07:35:45 +0400

    chg: registration

--- Commands/Registration/RegistrationCommand.cs
+++ Commands/Registration/RegistrationCommand.cs
@@ -11,43 +11,37 @@ namespace ServerAspNetCoreLinux.Commands.Registration
         private string _email { get; }
         private string _password { get; }
         private string _login { get; }
-        private string _permission { get; }
-        
-        private float _money { get; }
-        private float _hours_left { get; }
 
-        public RegistrationCommand(IFormCollection data, HttpContext httpContext) : base(data,httpContext, nameof(RegistrationCommand))
+        public RegistrationCommand(IFormCollection data, HttpContext httpContext) : base(data, httpContext, nameof(RegistrationCommand))
         {
             _email = data["email"];
             _password = data["password"];
             _login = data["login"];
-            _permission = data["permission"];
-            _money = (float) Convert.ToDouble(data["money"]);
-            _hours_left = (float) Convert.ToDouble(data["hours_left"]);
         }
 
         public override void Execute(ServerContext context)
         {
-            var userParam = new Random().Next(0, 100000).ToString();
-            
-            // UserParams.Add("userId", string.Empty);
-            // UserParams.Add("session", string.Empty);
+            try
+            {
+                if (context.UserModel.Emails.ContainsKey(_email))
+                {
+                    UserParams["error"] = true;
+                    UserParams["error_text"] = "Email exist";
+                }
+                else
+                {
+                    var user = new UserUnitModel(_email, _password, _login, 0, "user", 2);
 
-            if (context.UserModel.Emails.ContainsKey(_email))
-            {
-                UserParams["error"] = true;
-                UserParams["error_text"] = "Email exist";
+                    context.UserModel.Add(user);
+                    ServerLoggerModel.Log(TypeLog.UserMessage, $"user {_login} registered");
+                }
             }
-            else
+            catch (Exception e)
             {
-                // UserParams["userId"] = userParam;
-                // UserParams["session"] = userParam;
-                
-                var user = new UserUnitModel(_email, _password, _login, _money, _permission, _hours_left);
-                
-                context.UserModel.Add(user);
-                ServerLoggerModel.Log(TypeLog.UserMessage, $"user {userParam} registered");
+                Console.WriteLine(e);
+                throw;
             }
+
             Send();
         }
     }
