4b9bc455e9355180c1663570285a8ab6e0ccf3d7
phyzg0d <mephyz@mail.ru>
2020-11-28 07:54:53 +0400

    chg: sign in

--- Commands/SignIn_SignOut/UserSignInCommand.cs
+++ Commands/SignIn_SignOut/UserSignInCommand.cs
@@ -10,7 +10,7 @@ namespace ServerAspNetCoreLinux.Commands.SignIn_SignOut
         private string _email { get; }
         private string _password { get; }
 
-        public UserSignInCommand(IFormCollection data, HttpContext httpContext) : base(data,httpContext, nameof(UserSignInCommand))
+        public UserSignInCommand(IFormCollection data, HttpContext httpContext) : base(data, httpContext, nameof(UserSignInCommand))
         {
             _email = data["email"];
             _password = data["password"];
@@ -19,37 +19,39 @@ namespace ServerAspNetCoreLinux.Commands.SignIn_SignOut
         public override void Execute(ServerContext context)
         {
             UserParams.Add("permission", string.Empty);
-            
-            if (!context.UserModel.Emails.ContainsKey(_email))
+
+            try
             {
-                UserParams["error"] = true;
-                UserParams["error_text"] = "Wrong email";
-                
-                ServerLoggerModel.Log(TypeLog.UserMessage,"unsuccessful login attempt");
-            }
-            else
-            {
-                var userId = context.UserModel.Emails[_email];
-                var user = context.UserModel.Get(userId);
-                
-                if (_password == user.Properties.Get<string>("password").Value)
+                if (!context.UserModel.Emails.ContainsKey(_email))
                 {
-                    var userParam = new Random().Next(0, 100000).ToString();
-                    
-                    user.Properties.Get<string>("session").Value = userParam;
-                    
-                    UserParams["userId"] = user.Properties.Get<string>("id").Value;
-                    UserParams["session"] = userParam;
-                    
-                    ServerLoggerModel.Log(TypeLog.UserMessage,$"user {user.Properties.Get<string>("id").Value} logged in");
+                    UserParams["error"] = true;
+                    UserParams["error_text"] = "Wrong email";
+
+                    ServerLoggerModel.Log(TypeLog.UserMessage, "unsuccessful login attempt");
                 }
                 else
                 {
-                    UserParams["error"] = true;
-                    UserParams["error_text"] = "Wrong password";
-                    ServerLoggerModel.Log(TypeLog.UserMessage,$"user {user.Properties.Get<string>("id").Value} entered incorrect data");
+                    var userId = context.UserModel.Emails[_email];
+                    var user = context.UserModel.Get(userId);
+
+                    if (_password == user.Properties.Get<string>("password").Value)
+                    {
+                        ServerLoggerModel.Log(TypeLog.UserMessage, $"user {user.Properties.Get<string>("id").Value} logged in");
+                    }
+                    else
+                    {
+                        UserParams["error"] = true;
+                        UserParams["error_text"] = "Wrong password";
+                        ServerLoggerModel.Log(TypeLog.UserMessage, $"user {user.Properties.Get<string>("id").Value} entered incorrect data");
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
             Send();
         }
     }
