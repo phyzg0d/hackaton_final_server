4fd381b3085d79ae49d11fea8a523881a35a969b
phyzg0d <mephyz@mail.ru>
2020-11-29 07:19:37 +0400

    chg

--- Commands/AddTrackToHistoryCommand.cs
+++ Commands/AddTrackToHistoryCommand.cs
@@ -7,14 +7,16 @@ namespace ServerAspNetCoreLinux.Commands
     public class AddTrackToHistoryCommand : ExecuteCommand
     {
         private string _track { get; set; }
+        private string _userId { get; set; }
         public AddTrackToHistoryCommand(IFormCollection data, HttpContext httpContext) : base(data, httpContext, nameof(AddTrackToHistoryCommand))
         {
             _track = data["track"];
-            Console.WriteLine("ADDED");
+            _userId = data["user_id"];
         }
 
         public override void Execute(ServerContext context)
         {
+            context.UserModel[_userId].Properties.GetDictionary<string>("history").Value.Add(_track, "");
             Send();
         }
     }
